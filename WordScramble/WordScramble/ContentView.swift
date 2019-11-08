2d97c2093d9a8cd8ec1b7ca29ed7cc0c54055a9b
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2019-11-08 21:58:49 +0700

    [29] Project 5 part 1

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Kittisak Phetrungnapha on 8/11/2562 BE.
+//  Copyright © 2562 Kittisak Phetrungnapha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
