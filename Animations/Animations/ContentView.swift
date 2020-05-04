59bd0076b9526b17ecb6e15fca271a4fb9fc24aa
Kittisak Phetrungnapha <Kittisak.Phetrungnapha@agoda.com>
2020-05-04 16:15:03 +0700

    [32] Project 6, part 1

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Phetrungnapha, Kittisak (Agoda) on 4/5/2563 BE.
+//  Copyright © 2563 Phetrungnapha, Kittisak (Agoda). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        /*
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
+        */
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
