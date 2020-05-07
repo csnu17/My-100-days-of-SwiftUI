8ef5eed9c1f59e0b5edfb746c6d2894debe948e4
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2020-05-07 19:28:41 +0700

    [33][34] Project 6 part 2 and 3

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,19 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     
+    /*
     @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                self.animationAmount += 360
+//            }
+            self.enabled.toggle()
         }
         .padding(50)
-        .background(Color.red)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
         .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
         /*
         VStack {
             Stepper("Scale amount", value: $animationAmount.animation(
@@ -42,6 +47,92 @@ struct ContentView: View {
             .scaleEffect(animationAmount)
         }
         */
+    }
+    */
+    
+    /*
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            self.dragAmount = .zero
+                        }
+                }
+            )
+    }
+    */
+    
+    /*
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                }
+        )
+    }
+    */
+    
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
     }
 }
 
